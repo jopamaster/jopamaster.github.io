3d413469fcee4739a2efcd265f001b5eec52c58f
Pavle Jovanović <jopamaster@users.noreply.github.com>
2019-06-19 21:27:56 +0200

    Create intro.js

---
+++ TEST (RESPONSIVNESS)/intro.js
@@ -0,0 +1,11 @@
+
+ $(document).ready(function(){
+    $(this).scrollTop(0);
+
+    var dialog = document.getElementById("myDialog");
+	var close = document.getElementsByClassName("close-btn")[0];
+	$(window).on('load',function(){
+	$('#myDialog').dialog('show');});
+	close.onclick = function() {
+	dialog.style.display = "none";}
+});
