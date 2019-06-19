a3b00b5bdb7a3ba4c2a95183114a16fb269cfa0d
Pavle Jovanović <jopamaster@users.noreply.github.com>
2019-06-20 00:57:18 +0200

    Update intro.js

--- TEST (RESPONSIVNESS)/intro.js
+++ TEST (RESPONSIVNESS)/intro.js
@@ -1,7 +1,7 @@
 
  $(document).ready(function(){
     $(this).scrollTop(0);
-
+$.ui.dialog.prototype.options.responsive = true;
     var dialog = document.getElementById("myDialog");
 	var close = document.getElementsByClassName("close-btn")[0];
 	$(window).on('load',function(){
